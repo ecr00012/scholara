44c8dd1e3762ad0b9091d3c73d670febc4fff30c
ecr00012 <ecr00012@mix.wvu.edu>
2026-05-04 19:32:44 -0400

    Register all five custom commands in tauri invoke_handler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,7 @@
+mod commands;
+
+use commands::books::{app_data_dir_path, copy_uploaded_file, reveal_in_file_manager};
+use commands::secrets::{get_api_key, set_api_key};
 use tauri_plugin_sql::{Migration, MigrationKind};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -17,7 +21,13 @@ pub fn run() {
         )
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![
+            copy_uploaded_file,
+            app_data_dir_path,
+            reveal_in_file_manager,
+            get_api_key,
+            set_api_key,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
